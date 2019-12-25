5ce378241a977927814916fa3c4a925986447d57
zhouzhongyi1 <zhouzhongyi1@jd.com>
2019-12-25 20:56:49 +0800

    converter

--- spring-boot-web/src/main/java/notes/mvc/web/modules/ApplicationConfiguration.java
+++ spring-boot-web/src/main/java/notes/mvc/web/modules/ApplicationConfiguration.java
@@ -1,5 +1,8 @@
 package notes.mvc.web.modules;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.zaxxer.hikari.HikariDataSource;
 import notes.mvc.web.interceptor.ContextInterceptor;
 import notes.mvc.web.interceptor.LoginInterceptor;
@@ -9,11 +12,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.sql.DataSource;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Description：@Configuration自动扫描
@@ -56,6 +65,36 @@ public class ApplicationConfiguration extends WebMvcConfigurationSupport {
         //todo 重点：不能写成classpath:/static/**
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        //1.先定义一个convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //2.添加fastjson的配置信息，比如：是否要格式化返回的json数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullListAsEmpty
+        );
+
+        //处理中文乱码问题(不然出现中文乱码)
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastMediaTypes.add(MediaType.APPLICATION_JSON);
+        fastMediaTypes.add(new MediaType(MediaType.TEXT_PLAIN, Charset.forName("UTF-8")));
+        fastMediaTypes.add(new MediaType(MediaType.TEXT_HTML, Charset.forName("UTF-8")));
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+        //3.在convertzhong 添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        //4.将convert添加到converters当中
+        converters.add(fastConverter);
+
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        converters.add(converter);
+        super.configureMessageConverters(converters);
     }
 
     @Bean
