1a383fefd0febb04d1f9f6fc09431fef7d7102c4
zhouzhongyi1 <zhouzhongyi1@jd.com>
2019-12-30 20:16:44 +0800

    jvm参数调优

--- spring-boot-common/src/main/java/notes/mvc/common/TestMain.java
+++ spring-boot-common/src/main/java/notes/mvc/common/TestMain.java
@@ -5,10 +5,29 @@ import java.util.concurrent.TimeUnit;
 /**
  * Description：online conf
  *
+ * man
  * -Xmx6g -Xms6g -XX:+UseG1GC -XX:MetaspaceSize=512m -XX:G1MaxNewSizePercent=65
  * -XX:G1HeapRegionSize=32m -XX:MaxGCPauseMillis=500 -XX:G1HeapWastePercent=5
  * -XX:InitiatingHeapOccupancyPercent=10 -XX:ParallelGCThreads=1 -XX:ConcGCThreads=1
  * -XX:G1MixedGCLiveThresholdPercent=60 -Dfile.encoding=UTF-8 -XX:+UseStringDeduplication
+ *
+ * soa
+ * -Xmx6144M -Xms6144M -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -Xmn4g
+ * -XX:SurvivorRatio=6 -XX:MaxTenuringThreshold=5 -XX:GCTimeRatio=19 -Xnoclassgc
+ * -XX:+DisableExplicitGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection
+ * -XX:CMSFullGCsBeforeCompaction=0 -XX:-CMSParallelRemarkEnabled
+ *
+ * pc-langfang
+ * -Xmx6g -Xms6g -Xmn5g -XX:MetaspaceSize=512m -XX:MaxMetaspaceSize=1024m -XX:ParallelGCThreads=4
+ * -XX:ConcGCThreads=2 -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8
+ * -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseCMSInitiatingOccupancyOnly
+ * -XX:CMSInitiatingOccupancyFraction=60
+ *
+ * pc-huitian
+ * -Xmx6144M -Xms6144M -XX:PermSize=512M -XX:MaxPermSize=1024M -XX:+HeapDumpOnOutOfMemoryError
+ * -Dfile.encoding=UTF-8 -Xmn4g -XX:SurvivorRatio=6 -XX:MaxTenuringThreshold=5
+ * -XX:GCTimeRatio=19 -Xnoclassgc -XX:+DisableExplicitGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+ * -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:-CMSParallelRemarkEnabled
  *
  * @author zzy520git
  * @date 2019/12/30 18:08
